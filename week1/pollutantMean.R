04d0beac2bba17727d55474f8f57bbefe404d349
Asem Alaa <asem_alla@msn.com>
2019-09-24 00:22:20 +0200

    working pollutant

---
+++ week1/pollutantMean.R
@@ -0,0 +1,19 @@
+# install.packages("data.table")
+library("data.table")
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  
+  # Format number with fixed width and then append .csv to number
+  fileNames <- paste0(directory, '/', formatC(id, width=3, flag="0"), ".csv" )
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(fileNames, data.table::fread)
+  dt <- rbindlist(lst)
+  
+  if (c(pollutant) %in% names(dt)){
+    return(dt[, lapply(.SD, mean, na.rm = TRUE), .SDcols = pollutant][[1]])
+  } 
+}
+
+# Example usage
+m = pollutantmean(directory = 'data/pollution_data/specdata', pollutant = 'sulfate', id = 20)
