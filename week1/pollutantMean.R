39399f850f9e46f0c0ee56dcc151c4f512f94215
Asem Alaa <asem_alla@msn.com>
2019-09-24 00:23:38 +0200

    prepare demo

--- week1/pollutantMean.R
+++ week1/pollutantMean.R
@@ -4,15 +4,9 @@ library("data.table")
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   # Format number with fixed width and then append .csv to number
-  fileNames <- paste0(directory, '/', formatC(id, width=3, flag="0"), ".csv" )
-  
+
   # Reading in all files and making a large data.table
-  lst <- lapply(fileNames, data.table::fread)
-  dt <- rbindlist(lst)
-  
-  if (c(pollutant) %in% names(dt)){
-    return(dt[, lapply(.SD, mean, na.rm = TRUE), .SDcols = pollutant][[1]])
-  } 
+ 
 }
 
 # Example usage
